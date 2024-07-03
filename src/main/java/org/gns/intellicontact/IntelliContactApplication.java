aeea5aa814377b9ed4e363ef341cb8a8db1e7c5e
GNS VS <gnsgns6@outlook.com>
2024-07-03 21:22:03 +0530

    Created Contact and User Entity

---
+++ src/main/java/org/gns/intellicontact/IntelliContactApplication.java
@@ -0,0 +1,16 @@
+package org.gns.intellicontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliContactApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(IntelliContactApplication.class, args);
+        System.out.println("----------GNS-------------");
+
+    }
+
+}
---
+++ src/test/java/org/gns/intellicontact/IntelliContactApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gns.intellicontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliContactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
